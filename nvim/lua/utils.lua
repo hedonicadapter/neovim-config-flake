e2e7c8acad43d466166675095bc31a5ff655be1d
hedonicadapter <mailservice.samherman@gmail.com>
2024-09-24 06:55:43 +0200

    stuff

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,15 +1,3 @@
 local vim_fn = vim.fn
 
 _G.OS = (vim_fn.has("win32") == 0) and vim_fn.system("uname"):gsub("[\r\n]", "") or "Windows"
-
-_G.getFilesInDirectory = function(directory)
-	local files = {}
-	for dir in io.popen("ls -pa " .. directory .. " | grep -v /"):lines() do
-		local filename_withoutpercentage = string.gsub(dir, "%%", "/")
-		local filename = string.gsub(filename_withoutpercentage, "%.vim", "")
-		local fileObject = { name = filename }
-
-		table.insert(files, fileObject)
-	end
-	return files
-end
--- nvim/plugins/mini.lua
+++ nvim/plugins/mini.lua
@@ -1,10 +1,22 @@
 local starter = require("mini.starter")
 
-function merge_tables(t1, t2)
+local function merge_tables(t1, t2)
 	for k, v in ipairs(t2) do
 		table.insert(t1, v)
 	end
 	return t1
+end
+
+local getFilesInDirectory = function(directory)
+	local files = {}
+	for dir in io.popen("ls -pa " .. directory .. " | grep -v /"):lines() do
+		local filename_withoutpercentage = string.gsub(dir, "%%", "/")
+		local filename = string.gsub(filename_withoutpercentage, "%.vim", "")
+		local fileObject = { name = filename }
+
+		table.insert(files, fileObject)
+	end
+	return files
 end
 
 local my_items = function()
