20a35d3e1d78264bf4dd6e14d6362fcfb22714df
hedonicadapter <mailservice.samherman@gmail.com>
2024-10-14 15:26:28 +0200

    terraform autoformat

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -308,4 +308,11 @@ lspconfig.ansiblels.setup(coq.lsp_ensure_capabilities({
 	capabilities = capabilities,
 }))
 
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*.tf", "*.tfvars" },
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+})
+
 require("ufo").setup()
