54e4810814c22c3c948f22a4da83223aba517b4a
hedonicadapter <mailservice.samherman@gmail.com>
2024-12-26 13:41:41 +0100

    signature nvim removed to try out blink cmp signatures

--- flake.nix
+++ flake.nix
@@ -100,26 +100,6 @@
         }
 
         omnisharp-extended-lsp-nvim
-        {
-          plugin = lsp_signature-nvim;
-          config = toLua ''
-            vim.api.nvim_create_autocmd("LspAttach", {
-              callback = function(args)
-                local bufnr = args.buf
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if vim.tbl_contains({ 'null-ls' }, client.name) then  -- blacklist lsp
-                  return
-                end
-                require("lsp_signature").on_attach({
-                  bind = true,
-                  handler_opts = {
-                    border = "rounded"
-                  }
-                }, bufnr)
-              end,
-            })
-          '';
-        }
 
         promise-async
         {
@@ -135,19 +115,14 @@
           '';
         }
 
-        # cmp-cmdline
-        # cmp-nvim-lsp
-        # cmp-async-path
-        # cmp-buffer
-        # {
-        #   plugin = nvim-cmp;
-        #   config = toLuaFile ./nvim/plugins/cmp.lua;
-        # }
-
         {
           plugin = blink-cmp;
           config = toLua ''
-            require("blink.cmp").setup()
+            require("blink.cmp").setup({
+              signature = {
+                enabled = true,
+              }
+            })
           '';
         }
         {
