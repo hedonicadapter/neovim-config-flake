cabcbb8325b7cd208d377bede22111d08f688902
hedonicadapter <mailservice.samherman@gmail.com>
2025-02-09 13:23:38 +0100

    codecopmanion markdown fix maybe

--- flake.nix
+++ flake.nix
@@ -251,6 +251,17 @@
               preview = {
                 filetypes = { "markdown", "codecompanion" },
                 ignore_buftypes = {},
+                condition = function (buffer)
+                  local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt;
+
+                  if bt == "nofile" and ft == "codecompanion" then
+                    return true;
+                  elseif bt == "nofile" then
+                    return false;
+                  else
+                    return true;
+                  end
+                end,
               },
             })
           '';
