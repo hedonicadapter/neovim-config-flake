ae15c5c6f6e0878b5436ad91c2cbbc728eb0f251
hedonicadapter <mailservice.samherman@gmail.com>
2025-04-17 09:41:56 +0200

    completion docs andddddd kind/icons

--- flake.nix
+++ flake.nix
@@ -85,8 +85,40 @@
                 end
                 return true
               end,
+
               signature = {
                 enabled = true,
+              },
+
+              completion = {
+                documentation = {
+                  auto_show = true,
+                },
+
+                menu = {
+                  draw = {
+                    components = {
+                      kind_icon = {
+                        text = function(ctx)
+                          local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                          return kind_icon
+                        end,
+                        -- (optional) use highlights from mini.icons
+                        highlight = function(ctx)
+                          local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                          return hl
+                        end,
+                      },
+                      kind = {
+                        -- (optional) use highlights from mini.icons
+                        highlight = function(ctx)
+                          local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                          return hl
+                        end,
+                      }
+                    }
+                  }
+                }
               }
             })
           '';
