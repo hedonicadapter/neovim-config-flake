9c15e08936bded943c6b53be6987b520b04f5972
hedonicadapter <mailservice.samherman@gmail.com>
2025-06-27 22:37:02 +0200

    test auto-session lazy support

--- lua/luaConf/plugins/auto-session.lua
+++ lua/luaConf/plugins/auto-session.lua
@@ -3,13 +3,13 @@ return {
 		"auto-session",
 		for_cat = "general.always",
 		event = "VimEnter",
-		after = function(plugin)
+		after = function()
 			require("auto-session").setup({
 				auto_save = true,
 				auto_restore = true,
 				auto_restore_last_session = false,
 				log_level = "warn",
-				-- lazy_support = false,
+				lazy_support = false,
 				root_dir = vim.fn.stdpath("data") .. "/sessions/",
 			})
 		end,
